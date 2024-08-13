1abe30ec611ee6109c3bd4c2e2e6ddd13c312198
Emmanuel Urias <emmanuelurias60@icloud.com>
2024-08-13 12:54:49 -0700

    Deployed waiting for FetchSecret to finish

--- secrets-manager/db-integration/deploy.py
+++ secrets-manager/db-integration/deploy.py
@@ -8,8 +8,19 @@ def deploy():
         template_body = template_file.read()
 
     resp = cfn.create_stack(
-        StackName='secrets-manager-stack',
-        TemplateBody=template_body
+        StackName='secrets-manager-db-stack',
+        TemplateBody=template_body,
+        Parameters=[
+            {
+                'ParameterKey': 'VpcId',
+                'ParameterValue': 'vpc-00bb0ebfd33ff4f24'
+            },
+            {
+                'ParameterKey': 'Subnets',
+                'ParameterValue': 'subnet-0ad725ee29938aa51, subnet-025373b89d975735c'
+            }
+        ],
+        Capabilities=['CAPABILITY_IAM']
     )
     
     print("Stack creation initiated.")
