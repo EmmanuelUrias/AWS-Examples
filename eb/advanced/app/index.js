81c9567a338e73e1183b8c8de2eb8a985967a07d
Emmanuel Urias <emmanuelurias60@icloud.com>
2024-10-13 14:09:18 -0700

    Practice with EB

---
+++ eb/advanced/app/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const path = require('path')
+const port = process.env.PORT
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/index.html'))
+})
+
+app.get('/style.css', (req, res) => {
+    res.sendFile(path.join(__dirname, '/style.css'))
+})
+
+app.get('app.js', (req, res) => {
+    res.sendFile(path.join(__dirname, '/app.js'))
+})
+
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Example app listening on port ${port}`)
+})
